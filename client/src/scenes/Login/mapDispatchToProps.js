0e2d5660973305d5b90fd1a4ad1ad33a1fdcdf41
Dylan <dylanslack@gmail.com>
2016-12-16 12:48:38 -0700

    Remove pushTo from submit dispatcher

--- client/src/scenes/Login/mapDispatchToProps.js
+++ client/src/scenes/Login/mapDispatchToProps.js
@@ -3,6 +3,6 @@ import * as actions from '../../state/actions';
 export default dispatch => ({
   changeUsername: e => dispatch(actions.changeUsername(e.target.value)),
   changePassword: e => dispatch(actions.changePassword(e.target.value)),
-  submit: (username, password, pushTo) =>
-    dispatch(actions.loginRequest(username, password, pushTo)),
+  submit: (username, password) =>
+    dispatch(actions.loginRequest(username, password)),
 });
